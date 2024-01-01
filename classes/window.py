3ffb09e4207c72c7eac5fc439f737ff6ee1ebfab
Michael Franzl <michael@franzl.name>
2024-01-01 08:04:43 +0100

    Don\'t reset/abort on errors
    Because it causes grbl to go into an ALERT state: "abort during cycle"

--- classes/window.py
+++ classes/window.py
@@ -697,7 +697,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self._add_to_loginput("<b>◀ {}</b>".format(data[0]), "red")
             if data[2] > -1:
                 self._add_to_loginput("<b>✗ Error was in line {}: {}</b>".format(data[2], data[1]), "red")
-            self.reset()
 
         elif event == "on_alarm":
             self._add_to_loginput("☹ " + data[0], "orange")
